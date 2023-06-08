22a238dc9450177eb8d12751739498ae9c72f55f
lilaclilac09 <68252598+lilaclilac09@users.noreply.github.com>
2023-06-08 13:29:15 +0200

    Update LiquidityPool.sol

--- packages/hardhat/contracts/LiquidityPool.sol
+++ packages/hardhat/contracts/LiquidityPool.sol
@@ -10,30 +10,36 @@ import "./interfaces/IDAO.sol";
 contract LiquidityPool is ILiquidityPool, Ownable {
   IDAO DAO;
 
+  // Fallback function to receive ETH
   receive() external payable {
     emit Received(block.timestamp, msg.value);
   }
 
+  // Modifier to restrict access to only the DAO contract
   modifier onlyDAO() {
     require(msg.sender == address(DAO), "Only DAO can call this function.");
     _;
   }
 
+  // Sets the address of the DAO contract
   function setDAOAddress(address DAOAddress) external onlyOwner {
     DAO = IDAO(DAOAddress);
     renounceOwnership();
   }
 
+  // Sends ETH to a specified address, callable only by the DAO contract
   function sendEth(address payable _to, uint amount) external onlyDAO {
     require(address(this).balance >= amount, "Not enough balance.");
     _to.transfer(amount);
     emit Sent(_to, amount);
   }
 
+  // Returns the current balance of the liquidity pool
   function getBalance() external view returns (uint) {
     return address(this).balance;
   }
 
+  // Returns the address of the DAO contract
   function getDAOAddress() external view returns (address) {
     return address(DAO);
   }
