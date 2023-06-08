99eaac3928501feee06b05e71fba22b9ec6bf58d
lilaclilac09 <68252598+lilaclilac09@users.noreply.github.com>
2023-06-08 13:33:48 +0200

    Update IDAO.sol

--- packages/hardhat/contracts/interfaces/IDAO.sol
+++ packages/hardhat/contracts/interfaces/IDAO.sol
@@ -2,19 +2,36 @@
 
 pragma solidity ^0.8.10;
 
+/**
+ * @title IDAO Interface
+ * @dev Interface for the DAO contract.
+ */
 interface IDAO {
-  event Vote(address indexed voter, uint indexed proposalId, uint voteTime);
-  event CreateProposal(uint indexed proposalId, uint tokenId_, address beneficiaryAddress, uint createTime);
-  event Withdraw(address indexed user, uint stakingAmount, uint timestamp);
-  event Stake(address indexed user, uint stakingAmount, uint timestamp);
+    event Vote(address indexed voter, uint indexed proposalId, uint voteTime);
+    event CreateProposal(uint indexed proposalId, uint tokenId_, address beneficiaryAddress, uint createTime);
+    event Withdraw(address indexed user, uint stakingAmount, uint timestamp);
+    event Stake(address indexed user, uint stakingAmount, uint timestamp);
 
-  function createProposal(
-    uint tokenId_,
-    uint insuranceDuration,
-    uint insurancePremium,
-    uint claimTrigger,
-    address beneficiaryAddress
-  ) external;
+    /**
+     * @notice Create a new proposal.
+     * @param tokenId_ The token ID associated with the proposal.
+     * @param insuranceDuration The duration of the insurance for the proposal.
+     * @param insurancePremium The premium amount for the insurance.
+     * @param claimTrigger The trigger condition for claiming the insurance.
+     * @param beneficiaryAddress The address of the beneficiary for the insurance.
+     */
+    function createProposal(
+        uint tokenId_,
+        uint insuranceDuration,
+        uint insurancePremium,
+        uint claimTrigger,
+        address beneficiaryAddress
+    ) external;
 
-  function triggerInsurance(address _to, uint amount) external;
+    /**
+     * @notice Trigger the insurance by transferring the funds to the specified address.
+     * @param _to The address to transfer the insurance funds to.
+     * @param amount The amount of insurance funds to transfer.
+     */
+    function triggerInsurance(address _to, uint amount) external;
 }
